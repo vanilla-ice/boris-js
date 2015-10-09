48d82e3c9b1e039a1d9e3f4553bcbe527f77ee4e
vanilla-ice <valeraerohin97@mail.ru>
2015-10-10 02:52:05 +0300

    bower

---
+++ boris.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $.fn.boris = function() {
+    var fadefunc, outfunc;
+    $('<div class="boris" style="transform: translateX(-100%); position:fixed;"><img src="images/boris.jpg"><br><span class="txt" style="font-size:27px;">Работаешь, петух?))</span></div>').appendTo('body');
+    outfunc = function() {
+      return $('.boris').css({
+        'transform': 'translateX(-100%)',
+        'transition': 'all 0.6s ease'
+      });
+    };
+    fadefunc = function() {
+      var percent;
+      percent = 1;
+      $('.boris').css({
+        'margin-top': Math.floor(Math.random() * (30 - 0 + 1)) + 0 + "%",
+        'transform': 'translateX(0%)',
+        'transition': 'transform 0.6s ease'
+      });
+      return setTimeout(outfunc, 2500);
+    };
+    return setInterval(fadefunc, 5000);
+  };
+  return $('body').boris();
+});
